e1ba0b70945c8e579e03928c30b9ce665d3c278d
Daniel Welch <d.welch448@gmail.com>
2026-06-09 23:08:38 +0100

    allow toolchain stage select

--- scripts/001-ps3toolchain.sh
+++ scripts/001-ps3toolchain.sh
@@ -4,7 +4,25 @@ set -eu
 
 REPO_URL="${PS3TOOLCHAIN_REPO_URL:-https://github.com/ps3dev/ps3toolchain.git}"
 REPO_REF="${PS3TOOLCHAIN_REF:-master}"
-REPO_DIR="ps3toolchain"
+REPO_DIR="${PS3TOOLCHAIN_DIR:-ps3toolchain}"
+
+# Default: build every toolchain stage before PSL1GHT.
+#
+# Current ps3toolchain stages used here:
+#   1 2 4 5 6
+#
+# CI can override this, for example:
+#   PS3TOOLCHAIN_STAGES="1" sh ./build-all.sh
+#   PS3TOOLCHAIN_STAGES="1 2" sh ./build-all.sh
+#   PS3TOOLCHAIN_STAGES="1 2 4 5 6" sh ./build-all.sh
+#
+PS3TOOLCHAIN_STAGES="${PS3TOOLCHAIN_STAGES:-1 2 4 5 6}"
+
+echo "==> ps3toolchain"
+echo "REPO_URL=$REPO_URL"
+echo "REPO_REF=$REPO_REF"
+echo "REPO_DIR=$REPO_DIR"
+echo "PS3TOOLCHAIN_STAGES=$PS3TOOLCHAIN_STAGES"
 
 if [ ! -d "$REPO_DIR/.git" ]; then
   git clone --depth 1 --branch "$REPO_REF" "$REPO_URL" "$REPO_DIR"
@@ -15,5 +33,12 @@ fi
 
 cd "$REPO_DIR"
 
-# Build every toolchain stage before PSL1GHT
-./toolchain.sh 1 2 4 5 6
+echo "==> Host"
+uname -a || true
+uname -m || true
+
+echo "==> Building ps3toolchain stages: $PS3TOOLCHAIN_STAGES"
+
+# Intentional word splitting: stages are passed as individual arguments.
+# shellcheck disable=SC2086
+./toolchain.sh $PS3TOOLCHAIN_STAGES
